dcc63943842073ee966ad27b5ee045190cb9e1d5
Tushar Upadhyay <dev@tusharupadhyay.com>
2023-10-06 22:40:32 +0530

    Add project files.

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Notes.Models
+{
+    internal class Note
+    {
+        public string FileName { get; set; }    
+        public string Text { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
